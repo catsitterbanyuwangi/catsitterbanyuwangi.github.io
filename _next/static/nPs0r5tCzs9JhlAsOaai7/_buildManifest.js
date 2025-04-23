83fda90f54435478f88cf65618fcf0a900420203
akidraham <akidraham@users.noreply.github.com>
2025-04-23 13:11:45 +0000

    deploy: 43102e278cda9d5e6184c6c2a36718927be5fc92

---
+++ _next/static/nPs0r5tCzs9JhlAsOaai7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-82c8287d732697b1.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
